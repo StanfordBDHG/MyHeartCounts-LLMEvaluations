b6eccf93081defa8bc2d5d140c638138f8255a69
Andy Wang <106629086+awangwang@users.noreply.github.com>
2026-02-02 00:11:47 -0800

    update copyright licenses

--- nudge-generation/config/models.ts
+++ nudge-generation/config/models.ts
@@ -1,10 +1,10 @@
-/** 
- This source file is part of the Stanford Biodesign Digital Health MyHeart Counts open-source project
-
- SPDX-FileCopyrightText: 2025 Stanford University
-
- SPDX-License-Identifier: MIT
-*/
+//
+// This source file is part of the Stanford Biodesign Digital Health MyHeart Counts open-source project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025-2026 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 
 import { type ModelProvider } from '../backends/ModelBackend.js'
 
